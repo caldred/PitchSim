4fb359ca27ffad676d27338b27f0a8bb38d94bad
Cal Aldred <cal.aldred@gmail.com>
2023-08-10 22:16:53 -0700

    includes changes in run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import joblib
 
 from data_loader import PitchDataLoader
 import processing as prc
@@ -44,3 +45,24 @@ rv = pd.read_csv('runvalue.csv')
 
 stuff.simulate_pitches(df, batch_size, n_batches, platoon_cluster_dist, 
                        feat, rv, xgb_models, path, year=2022)
+
+simR = []
+simL = []
+for i in range(n_batches):
+    simR.append(pd.read_parquet(f'data/sim_vsR_batch{i+1}.parquet'))
+    simL.append(pd.read_parquet(f'data/sim_vsL_batch{i+1}.parquet'))
+simR = pd.concat(simR).reset_index(drop=True)
+simL = pd.concat(simL).reset_index(drop=True)
+
+distill_features = ['speed', 'speed_diff', 'lift', 'lift_diff', 
+                    'transverse', 'transverse_pit', 'transverse_pit_diff', 
+                    'release_pos_x', 'release_pos_x_pit', 'release_pos_y', 'release_pos_z', 
+                    'vert_approach_angle_adj'] + stuff.cluster_names
+events = [x for x in simR.columns if x.startswith('x_')]
+vsR_models = stuff.train_distilled_models(simR, distill_features, events)
+vsL_models = stuff.train_distilled_models(simL, distill_features, events)
+
+df = stuff.make_distilled_predictions(df, distill_features, events, vsR_models, vsL_models, game_year=2022)
+df.to_parquet(f'{path}pitch_data_with_predictions.parquet')
+
+stuff.generate_results_csv(df, distill_features)
